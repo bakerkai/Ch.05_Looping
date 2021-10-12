803aa78dd542e8385fab7ce85cb0b59294ae847e
bakerkai <bakerkai@urbandaleschools.com>
2021-10-12 10:33:10 -0500

    12123

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,10 +6,12 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
+import random
+heads += 0
+my_number = random.randrange(1, 3)
+#if my_number == 1:
 
-
-
-
+print(my_number)
 
 
 
--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -4,11 +4,20 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 
 '''
+print("Welcome to the camel game (totally not a copy)")
+print("You and your camel are running from natives in the desert")
+print("You only have a certain range of miles you can travel without food and water, so be prepared and be smart.")
+
+#start of sequence
+
+foodamt =
 
 
 
 
-
+quit_game = input("In order to quit Type Y or N")
+if quit_game == 'Y':
+    quit()
 
 
 
