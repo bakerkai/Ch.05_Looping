69908289d9b7fb0dc051555bce64b5df249c9ffb
bakerkai <bakerkai@urbandaleschools.com>
2021-10-18 12:41:59 -0500

    j

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -14,17 +14,20 @@ done = False
 print("Welcome to the camel game (totally not a copy)")
 print("You and your camel are running from natives in the desert")
 print("You only have a certain range of miles you can travel without food and water, so be prepared and be smart.")
-
+print("You must reach your house before the wolves catch you. ")
 #setting attributes
 foodamt = random.randint(10,15)
 health = int(50)
 wolfdistance = int(50)
 healthdecrease = random.randint(5, 15)
 randomrunrandomwolf = random.randint(5, 15)
-randomrun = random.randint(5, 15)
+randomrun = random.randint(10, 15)
 sleepheal = int(10)
 randomwalk = random.randint(5, 10)
 easyhealthdecrease = random.randint(99, 100)
+distancefromhome = 50
+hungerdecrease = random.randint(5, 10)
+foodscavange = random.randint(0, 5)
 #start of sequence
 if wolfdistance == 0:
     done = True
@@ -33,7 +36,7 @@ if health <= 0:
     done = True
 while not done:
     print("What do you want to do?")
-    userinput = input("Do you want to, A: check status, B: Sleep, C: Eat, D: Sprint till ya cant no more, E: Moderate walk, Q: Quit game")
+    userinput = str(input("Do you want to, A: check status, B: Sleep, C: Eat, D: Sprint till ya cant no more, E: Moderate walk, Q: Quit game, X:Scavange for food"))
     if userinput == 'A':
         print("Health", health, "Food", foodamt)
     elif userinput == 'C':
@@ -45,6 +48,8 @@ while not done:
         wolfdistance = wolfdistance + randomrun
         wolfdistance = wolfdistance - randomrunrandomwolf
         health = health - healthdecrease
+        distancefromhome = distancefromhome - randomrun
+        foodamt = foodamt - hungerdecrease
     elif userinput == 'B':
         print("You have decided to stay and sleep. Your health increased by", sleepheal, " However the wolves have advanced by", randomrunrandomwolf)
         health = health + sleepheal
@@ -54,17 +59,21 @@ while not done:
         health = health - easyhealthdecrease
     elif userinput == 'Q':
         quit()
+    elif userinput == 'X':
+        print("You have chosen to scavange for food. Whilst doing this the wolves advanced by", randomrunrandomwolf, "But you have gotten", )
+        wolfdistance = wolfdistance - randomrunrandomwolf
+        foodamt = foodamt + foodscavange
+    if wolfdistance <= 20:
+        print("WARNING the hungry dogs are getting close")
+    if distancefromhome <= 20:
+        print("WARN- oops i meant good job you are close to home! keep going")
 
 
 
 '''
 CURRENT BUGS:
-Need random number removals (EX health) to actually apply 
-Random.randint(X,X) selections are not applied correctly (not a random num)
+Need random number removals (EX health) to actually apply -  done
 Program does not quit when health is below 0
-add way to eat food
-sleep night 
-direct quit command 
 needs finish line 
 just add more things in general to it to make it more usable 
 add a warning if they get close 
